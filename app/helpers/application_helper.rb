efdff108054eebcb8d6e41335014493bff2483aa
Andy Duss <mindovermiles262@gmail.com>
2017-09-14 08:47:36 -0700

    added dynamic samples in project form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
 module ApplicationHelper
-  def helper_to_add_test(name, f, association)
+  def link_to_add_field(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
+
+  def button_to_add_field(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    button_to(name, '#', class: "add_fields button is-info", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
