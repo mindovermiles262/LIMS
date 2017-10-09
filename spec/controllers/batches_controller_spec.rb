fda7afa65b82605013263674ea1e86d0b4bd98a6
Andy Duss <mindovermiles262@gmail.com>
2017-10-09 08:33:54 -0700

    batches_controller specs #show

--- spec/controllers/batches_controller_spec.rb
+++ spec/controllers/batches_controller_spec.rb
@@ -15,7 +15,76 @@ RSpec.describe BatchesController, type: :controller do
       get :index
       expect(response).to redirect_to new_user_session_path
     end
-    it 'checks user is admin or analyst'
-    it 'checks for cancel'
+    it 'checks user is admin or analyst' do
+      sign_out :all
+      sign_in(FactoryGirl.create(:user))
+      get :index
+      expect(response).to redirect_to root_path
+      sign_out :user
+      sign_in(FactoryGirl.create(:analyst))
+      get :index
+      expect(response).to render_template :index
+      sign_out :analyst
+      sign_in(FactoryGirl.create(:admin))
+      get :index
+      expect(response).to render_template :index
+    end
+    it 'checks for cancel' do
+      sign_in(FactoryGirl.create(:analyst))
+      post :create, params: { test_method: TestMethod.first, commit: "Cancel" }
+      expect(response).to redirect_to batches_path
+      expect{response}.to change{Batch.count}.by (0)
+    end
+  end
+
+  describe '#index' do
+    it 'finds all batches' do
+      sign_in(FactoryGirl.create(:analyst))
+      get :index
+      expect(assigns(:batches).count).to eql(1) 
+    end
+  end
+
+  describe '#show' do
+    before :each do
+      sign_in(FactoryGirl.create(:analyst))
+    end
+    it 'finds the batch' do
+      get :show, params: { id: @batch.id }
+      expect(assigns(:batch)).to eql(@batch)
+    end
+    it 'populates the pipets' do
+      @pipet = FactoryGirl.create(:pipet)
+      @batch.pipets = [@pipet]
+      get :show, params: { id: @batch.id }
+      expect(assigns(:pipets)).to eql("P#{@pipet.id}")
+    end
+  end
+
+  describe '#new' do
+    it 'sets available methods'
+    it 'sets batch'
+    it 'builds tests'
+  end
+
+  describe '#create' do
+    context 'with valid parameters' do
+      it 'creates new batch'
+      it 'renders flash'
+      it 'redirects to edit batch'
+    end
+    context 'with invalid parameters' do
+      it 'does not save new batch'
+      it 'flashes danger'
+      it 'renders new template'
+    end
+  end
+
+  describe '#add' do
+    it 'finds the test' 
+    it 'finds the batch'
+    it 'add test with valid parameters'
+    it 'flashes danger with invalid params'
+    it 'redirects with invalid params'
   end
 end
