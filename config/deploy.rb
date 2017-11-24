8029850acb5acb86b1818635c6727e6b2249d709
Andy Duss <mindovermiles262@gmail.com>
2017-11-24 12:44:51 -0800

    set deploy location

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:mindovermiles262/appert.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/appert"
+set :deploy_to, "/home/student/www/appertlims.com"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
